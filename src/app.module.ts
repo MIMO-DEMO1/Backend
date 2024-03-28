54675a0b0ed580ca13ed300c70d8a641d0a49fcc
Huuthien1404 <thie1232135@gmail.com>
2024-03-28 19:40:29 +0700

    fix

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,19 +4,24 @@ import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { CacheModule } from '@nestjs/cache-manager';
-import { redisStore } from 'cache-manager-redis-yet';
+import * as redisStore from 'cache-manager-redis-store';
 import { CloudinaryModule } from './cloudinary/cloudinary.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
-    AuthModule,
-    UserModule,
+    ConfigModule.forRoot(),
     CacheModule.register({
       isGlobal: true,
       store: redisStore,
       host: process.env.REDIS_STORE_HOST,
       port: Number(process.env.REDIS_STORE_PORT),
+      username: process.env.REDIS_USERNAME,
+      password: process.env.REDIS_PASSWORD,
+      no_ready_check: true,
     }),
+    AuthModule,
+    UserModule,
     CloudinaryModule,
   ],
   controllers: [AppController],
